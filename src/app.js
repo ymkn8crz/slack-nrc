bc8099c81d1790560d369e7a2c72ca47ac84fb79
ymkn8crz <nofollow0123@gmail.com>
2023-03-21 00:08:55 +0900

    add oauth token flow

--- src/app.js
+++ src/app.js
@@ -8,6 +8,46 @@ const record = require('./modules/record');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'my-state-secret',
+  scopes: [
+   'channels:read',
+   'users:read',
+   'chat:write',
+   'channels:history',
+   'commands',
+   'im;history'
+  ],
+  installationStore: {
+    storeInstallation: async (installation) => {
+      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
+        return await database.set(installation.enterprise.id, installation);
+      }
+      if (installation.team !== undefined) {
+        return await database.set(installation.team.id, installation);
+      }
+      throw new Error('Failed saving installation data to installationStore');
+    },
+    fetchInstallation: async (installQuery) => {
+      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+        return await database.get(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        return await database.get(installQuery.teamId);
+      }
+      throw new Error('Failed fetching installation');
+    },
+    deleteInstallation: async (installQuery) => {
+      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+        return await database.delete(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        return await database.delete(installQuery.teamId);
+      }
+      throw new Error('Failed to delete installation');
+    },
+  },
 });
 
 const main = async() => {
